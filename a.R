78556fbd5bc6d5917dbacf7f22ee5f6f6604a704
KristineBirk <65944478+KristineBirk@users.noreply.github.com>
2023-11-17 11:34:02 +0100

    Update a.R

--- a.R
+++ a.R
@@ -1,3 +1,12 @@
-library(tidyverse)
-library(ggplot2)
-library(vegan)
+### Dragon analysis ###
+
+library("broom")
+
+# Import data
+source("R/import_dragon_data.R")
+
+
+# Run model
+mod <- lm(body_size ~ body_length * temperature, data = dragon)
+
+tidy(mod)
